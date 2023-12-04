aacc2d5fd5d4a3860ef7c1661698a92c26d7ed14
IC3P3 <n.eifler@freenet.de>
2023-12-04 10:56:33 +0100

    Reading file from resources

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let _input = read_vector_from_file(&String::from("resources/input"));
+}
+
+fn read_vector_from_file(filename: &String) -> Vec<String> {
+    read_to_string(filename)
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .collect()
 }

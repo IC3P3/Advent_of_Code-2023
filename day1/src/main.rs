510a514e7c6b5b3a02e45edf443605b207aaa4d3
IC3P3 <n.eifler@freenet.de>
2023-12-04 10:56:33 +0100

    Added functions to calculate calibration value

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,6 +2,8 @@ use std::fs::read_to_string;
 
 fn main() {
     let _input = read_vector_from_file(&String::from("resources/input"));
+
+    println!("{:?}", _input);
 }
 
 fn read_vector_from_file(filename: &String) -> Vec<String> {
@@ -11,3 +13,15 @@ fn read_vector_from_file(filename: &String) -> Vec<String> {
         .map(String::from)
         .collect()
 }
+
+fn get_combined_calibration_value(input_list: &Vec<String>) -> u64 {
+    let mut calibration_value = 0;
+
+    for line in input_list {
+        calibration_value += get_calibration_value(line);
+    }
+
+    calibration_value
+}
+
+fn get_calibration_value(input_list: &String) -> u64 {}
